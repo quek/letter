29bc47d1e1a1bfb859fab64845906871fb58980f
TAHARA Yoshinori <read.eval.print@gmail.com>
2015-11-22 23:05:40 +0900

    eval-when

--- page/public/public.lisp
+++ page/public/public.lisp
@@ -2,13 +2,14 @@
 
 (named-readtables:in-readtable info.read-eval-print.double-quote:|#"|)
 
-(defparameter *analytics-code*
-  "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defparameter *analytics-code*
+    "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 ga('create', 'UA-69873049-1', 'auto');
-ga('send', 'pageview');")
+ga('send', 'pageview');"))
 
 (defmacro with-public-template ((&key (title "letter")) &body contents)
   `(html
